4beb66cae975bdbf8853e75678777e76d442eba8
Алтынбек Турсункулов <ooo.doom00@gmail.com>
2021-07-02 23:58:07 +0600

    Add project files.

---
+++ RossetaStone/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RossetaStone.Data;
+
+namespace RossetaStone.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class TestController : Controller
+    {
+        [HttpGet("admin")]
+        [Authorize(Roles = RoleNames.Administrator)]
+        public IActionResult Get()
+        {
+            return Ok("Cтраница администратора");
+        }
+    }
+}
---
+++ RossetaStone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RossetaStone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
