c1380052ea6c956e0c99af8a7a34911713e19e47
Алтынбек Турсункулов <ooo.doom00@gmail.com>
2021-07-07 00:37:52 +0600

    Datatale_V2

---
+++ RossetaStone/Models/Dictionary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RossetaStone.Models
+{
+    public class Dictionary
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DictionaryId { get; set; } = 0;
+        public string WordEng { get; set; } = "";
+        public string WordRus { get; set; } = "";
+    }
+}
